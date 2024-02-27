f0985166d8d9e5ed0a9c486d6fe91c20ea3e0613
Ahsan <154538505+ahsan0311@users.noreply.github.com>
2024-02-27 10:47:50 +0500

    js-chp-1

---
+++ chapter-1/app.js
@@ -0,0 +1,18 @@
+
+// ALERT
+alert("Eror!,Please Enter a Valid password");
+alert("Welcome to js land \n Hapy Coding");
+alert("wlcome to js land");
+alert("HAPPY CODING \n Prevent this page from creating additional dailog");
+alert("Hello! I Run JS through my web,browser console");
+console.log("Hello! I Run JS through my web,browser console");
+// ALERT END
+
+
+// SAWL
+swal("Eror!,Please Enter a Valid password");
+swal("Welcome to js land \n Hapy Coding");
+swal("wlcome to js land");
+swal("HAPPY CODING \n Prevent this page from creating additional dailog");
+swal("Hello! I Run JS through my web,browser console");
+// SAWL END
