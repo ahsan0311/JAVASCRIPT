ddd389cba4df96258eebddea39fb1d9a2af10930
Ahsan <154538505+ahsan0311@users.noreply.github.com>
2024-03-01 12:12:34 +0500

    chp 2 and 3

---
+++ chapter-2 and 3/app.js
@@ -0,0 +1,36 @@
+let userName = ("ahsan123");
+console.log(userName);
+
+let myName = ("AhsanKhan");
+console.log(myName);
+
+// let js = ("Hello World");
+// alert(js);
+
+// alert("jhonsena");
+// alert("15years old");
+// alert("certified mobile app devlopment");
+
+// alert(" PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// let email =("ahsan@gmail.com");
+// alert(email);
+
+// let book = ("A Smart Way To Learn Javascript");
+// alert(book);
+
+// let age =("i am 18 years old");
+// alert(age);
+
+// let visit = ("you have this site 15 times");
+// alert(visit);
+
+let content = ("Yeah! I can write HTML Content Through Javascript , <br/>");
+document.write(content);
+
+let birth_year = ("My birth year is 2004 , <br/>");
+document.write(birth_year);
+
+let abc_Gamrment = ("JhonBhai ordered 5 T-shirt(s) on ABC Garments");
+document.write("BuyerName : ", abc_Gamrment   )
+
