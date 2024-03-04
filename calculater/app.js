60013eb43c6174440db6b1d289a980db19e26e0c
Ahsan <154538505+ahsan0311@users.noreply.github.com>
2024-03-04 11:47:23 +0500

    Calculater

---
+++ calculater/app.js
@@ -0,0 +1,18 @@
+// Calculater
+
+
+
+let num1 = +prompt('Enter your first number');
+let num2 = +prompt('Enter your Second number');
+
+let sum = num1 + num2
+document.write ("You have added two number than your answer is " , sum ,"<br/>");
+
+let divide = num1 - num2
+document.write ("You have divided two number than your answer is " , divide ,"<br/>");
+
+let multiply = num1 / num2
+document.write ("You have multiplied two number than your answer is " , multiply ,"<br/>");
+
+let reminder = num1 % num2
+document.write ("You have used reminder method two number than your answer is " , reminder ,"<br/>");
