a4d5f33ea98a7a56e5a115bae9ed06083484e168
Ahsan <154538505+ahsan0311@users.noreply.github.com>
2024-03-11 19:23:56 +0500

    chapter 4-5

---
+++ chap-4and5/app.js
@@ -0,0 +1,121 @@
+//                 // CHAPTER-4
+// console.log("<h1>Rules for naming  JS variable<h1/>" ,"<br>");
+// console.log("variable name must contain number , $ , _ for example : ","<br> <br>");
+// console.log("ayanAli :this is camel case" ,"<br><br>")
+// console.log("ayan_ali : this is snake case" ,"<br> <br>");
+// console.log("Variable names are case sensitive" ,"<br> <br>");
+// console.log("you can not use Javasrcipt reserved keywords" ,"<br>")
+// console.log("For example : console , Alert , function etc....")
+//                 // END
+
+
+//                 // CHAPTER-5
+
+
+        // Question-1
+
+// let num_1 = +prompt("Enter your first number");
+// let num_2 = +prompt("Enter your second number");
+
+// let sum = num_1 + num_2;
+// console.log("Sum of two numbers ",sum ,"<br>");
+
+// let subtract = num_1 - num_2;
+// console.log("Subraction of two numbers ",subtract ,"<br>" );
+
+// let multiply = num_1 * num_2;
+// console.log("Multiplication of two numbers ",multiply ,"<br>" );
+
+// let divide = num_1 / num_2;
+// console.log("Division of two numbers ",divide ,"<br>");
+
+// let modules = num_1 % num_2;
+// console.log("Reminder and Modules of two numbers ",modules ,"<br>");
+
+
+        // Question-2
+// let one_ticketprice = ("600 PKR");
+// let ticket_qut = +prompt("How many tickets do you want to buy ?");
+// let total_price_of_tickets = (600 * ticket_qut );
+// document.write("Total price of tickets you have bought is ",total_price_of_tickets ,"PKR");
+
+
+        // Question-3
+
+//  let table = +prompt("Enter your fav number");
+//  document.write(table , " x " , " 1 " , " = " , table * 1 , "<br>");       
+//  document.write(table , " x " , " 2 " , " = " , table * 2 , "<br>");       
+//  document.write(table , " x " , " 3 " , " = " , table * 3 , "<br>");       
+//  document.write(table , " x " , " 4 " , " = " , table * 4 , "<br>");       
+//  document.write(table , " x " , " 5 " , " = " , table * 5 , "<br>");       
+//  document.write(table , " x " , " 6 " , " = " , table * 6 , "<br>");       
+//  document.write(table , " x " , " 7 " , " = " , table * 7 , "<br>");       
+//  document.write(table , " x " , " 8 " , " = " , table * 8 , "<br>");       
+//  document.write(table , " x " , " 9 " , " = " , table * 9 , "<br>");       
+//  document.write(table , " x " , " 10 " , " = " , table * 10 , "<br>");
+
+        // question-4
+
+// let price_of_item_1 = +prompt("Enter your item-1 price");
+// let price_of_item_2 = +prompt("Enter your item-2 price");
+// let quantity_of_item_1 = +prompt("Enter your item-1 quantity");
+// let quantity_of_item_2 = +prompt("Enter your item-2 quantity");
+// let shipping_charges = 150;
+  
+
+// let total_cost = ( (price_of_item_1 * quantity_of_item_1) + (price_of_item_2 * quantity_of_item_2) +150);
+// console.log("item-1 price is ",  price_of_item_1 , "<br>");
+// console.log("item-1 quantity is ",  quantity_of_item_1  , "<br>");
+// console.log("item-2 price is ",  price_of_item_2  , "<br>");
+// console.log("item-2 quantity is ",  quantity_of_item_2 , "<br>");
+// console.log("Total price of your order is" , total_cost );
+
+            // question-5
+
+
+//  let obtained_marks = +prompt("Enter your obtained marks");          
+//  let total_marks = +prompt("Enter your total marks"); 
+ 
+//  let percentage = (obtained_marks/total_marks*100);
+//  console.log("your obtained marks is", obtained_marks ,"<br>" );
+//  console.log("your total marks is", total_marks ,"<br>");
+//  console.log("According to your marks your percentage is ", percentage );
+
+            // question-6
+
+// let num = (8);
+// document.write(num + 5 , "<br>", num *10 , "<br>", num /2);
+
+                // question-7
+
+
+// let current_year = +prompt("enter your current age year");
+// let birth_year = +prompt("enter your birth year");
+// let age_calculater = (current_year - birth_year )
+
+//  document.write("<h1>Age Calculater<h1/>");
+
+//  document.write("current_year ",current_year ,"<br>" );
+//  document.write("birth_year ",birth_year ,"<br>");
+//  document.write("You are  ", age_calculater , " years old");
+
+ 
+        // question-8
+
+// let fav_snack = prompt("Enter your favourite snack");
+// let current_age = +prompt("Enter your current age");
+// let est_max_age = +prompt("Enter your estimated maximum age");
+// let per_day_amount = +prompt("Enter your per day snacks amount");
+
+//  document.write("<h1> The Lifetime Supply Calculater <h1/>");
+//  document.write("Your favourite snack is " , fav_snack , "<br>");
+//  document.write("Your current age" , current_age , "<br>");
+//  document.write("Your estimated maximum age  is" , est_max_age , "<br>");
+//  document.write("Your  per day snacks amount is" , per_day_amount , "<br>");
+//  document.write("You will need 150 " , fav_snack , "to last you until the ripe old age of " , est_max_age);
+
+
+
+        
+ 
+            
