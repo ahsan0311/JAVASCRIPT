1fba0961246d1794215d6fa1ac3339a171b00677
Ahsan <154538505+ahsan0311@users.noreply.github.com>
2024-03-21 06:36:22 +0500

    chap-6

---
+++ app.js
@@ -0,0 +1,51 @@
+// let a = 10;
+// console.log("The value of a is", a);
+// let a_1 = ++a;
+// console.log("After increment the value of a is ", a_1);
+
+// let b = 10;
+// console.log("The value of a is", b);
+// let b_1 = --b;
+// console.log("After decrement the value of b is ", b_1);
+
+        // question-2
+
+//  let a  = 2;
+//  let b = 1;
+//  console.log(--a - --b + ++b + ++a);
+ 
+//  console.log("--a =" , --a);
+//  console.log("--a - --b =" , --a - --b);
+//  console.log("--a - --b + ++b =" , --a - --b + ++b );
+//  console.log("--a - --b + ++b + b++ =" , --a - --b + ++b + b++ );
+
+        // question-3
+
+//  let table = +prompt("Enter your fav number", 5);
+//  console.log(table , " x " , " 1 " , " = " , table * 1 , "<br>");       
+//  console.log(table , " x " , " 2 " , " = " , table * 2 , "<br>");       
+//  console.log(table , " x " , " 3 " , " = " , table * 3 , "<br>");       
+//  console.log(table , " x " , " 4 " , " = " , table * 4 , "<br>");       
+//  console.log(table , " x " , " 5 " , " = " , table * 5 , "<br>");       
+//  console.log(table , " x " , " 6 " , " = " , table * 6 , "<br>");       
+//  console.log(table , " x " , " 7 " , " = " , table * 7 , "<br>");       
+//  console.log(table , " x " , " 8 " , " = " , table * 8 , "<br>");       
+//  console.log(table , " x " , " 9 " , " = " , table * 9 , "<br>");       
+//  console.log(table , " x " , " 10 " , " = " , table * 10 , "<br>");        
+
+            // question-5
+
+//  let Math_marks = +prompt("Enter your Math subject marks");           
+//  let Urdu_marks = +prompt("Enter your Urdu subject marks");           
+//  let English_marks = +prompt("Enter your English subject marks");  
+//  let total_marks = 300;
+//  let obtained_marks = Math_marks+Urdu_marks+English_marks;
+
+ 
+//  let percentage  = obtained_marks/total_marks*100;
+
+//  console.log ("English 100 out of" , English_marks);
+//  console.log ("Math 100 out of" , Urdu_marks);
+//  console.log ("Urdu 100 out of" , Math_marks);
+//  console.log("according to your marks your is ", percentage , "%");
+
