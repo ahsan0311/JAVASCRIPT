2d4bb111984ffa6c2261a8ac602cb1f350b8e688
Ahsan <154538505+ahsan0311@users.noreply.github.com>
2024-03-28 06:55:59 +0500

    chapter 9-11

---
+++ chap-9-11/app.js
@@ -0,0 +1,152 @@
+// let city = prompt("Enter your favourite city of pakistan");
+//  if(city === "karachi"){
+//     console.log("Welcome to city of light");
+//  }
+//  else{
+//     console.log("Tu Mera Pootar Chooti Kr ");
+//  }
+
+
+               // question-2
+
+// let gender = prompt("Enter your gender");
+// if(gender === "male"){
+//    console.log("Good Morning Sir")
+// }
+// else if(gender === "female"){
+//    console.log("Good Morning Ma'am")
+// }
+
+// else{
+//    console.log("Ilaj krwa ")
+// }
+
+
+            // question-3
+
+// let signal = prompt("Enter your signal color");
+// if(signal === "red"){
+//    console.log("ABHI SABBAR KARO")
+// }
+
+// else if(signal === "yellow"){
+//    console.log("NIKALNY K LIA READY RAHO ")
+// }
+
+// else if(signal === "green"){
+//    console.log("CHALO NIKAL JAO")
+// }
+
+// else{
+//    console.log("PHELE DRIVING SIKH")
+// }
+      
+
+         // question-4
+
+//  let fuel = prompt("How many litres of fuel in your car ");
+//  if(fuel < 0.25){
+//    console.log("Please refill the fuel in yoyr car")
+//  }
+
+//  else{
+//    console.log("Get up it's 7am ")
+//  }
+
+
+                // question-5
+
+
+// let urdu = +prompt("Enter your Urdu marks (out of 100):");               
+// let english = +prompt("Enter your English marks (out of 100):");               
+// let math = +prompt("Enter your Math marks (out of 100):"); 
+                
+// if (urdu > 100 || english > 100 || math > 100) {
+//     console.log("Marks cannot be greater than 100. Please enter valid marks.");
+// } else {
+// let obtained_marks = urdu + english + math;
+// let total_marks = 300;
+// let percentage = (obtained_marks / total_marks) * 100;
+                                
+// console.log("Total Marks: ", total_marks);
+// console.log("Obtained Marks: ", obtained_marks);
+// console.log("Your percentage is: ", percentage.toFixed() + "%");
+                
+// if (percentage >= 80 ) {
+//     console.log("According to your percentage, your grade is 'A-one'.");
+//     console.log("Remarks: Excellent");
+// } else if (percentage >= 70 ) {
+//     console.log("According to your percentage, your grade is 'A'.");
+//     console.log("Remarks: Good");
+// } else if (percentage >= 60 ) {
+//     console.log("According to your percentage, your grade is 'B'.");
+//     console.log("Remarks: You need to work harder");
+// } else if (percentage >= 50 ) {
+//      console.log("According to your percentage, your grade is 'C'.");
+//      console.log("Remarks: You need to improve");
+// } else {
+//     console.log("According to your percentage, your grade is 'Fail'.");
+//     console.log("Remarks: Parhai Likhai Me Dehan Lagao Beta");
+// }
+// }
+                               
+
+
+
+            // question-6
+// let secret_number = 6;
+// let number = +prompt("Enter your guess (a number from 1 to 10):");
+            
+// if (number >= 1 && number <= 10) {
+//     if (secret_number === number) {
+//     console.log("Bingo! Correct answer");
+// } else {
+//     console.log("Close enough to the correct answer.");
+// }
+// } else {
+//     console.log("Please enter a number between 1 and 10.");
+// }
+ 
+
+            // question-7
+// let number = +prompt("Enter a number");
+
+// if (number % 2 === 0) {
+//     console.log("EVEN");
+// } else {
+//     console.log("ODD");
+// }        
+
+
+//             // question-8
+// let number = +prompt("Enter a number");
+
+// if (number % 3 === 0) {
+//     console.log(number , " is divisible by 3 and the ansver is " , number%3);
+// } else {
+//     console.log(number , " is not divisible by 3");
+// }
+
+                // question-9
+
+// let temp = +prompt("Enter today temperature");
+// if(temp>=10 && temp<20){
+//     console.log("“OMG! Today’s weather is so Cool.”");
+// }
+// else if(temp>=20 && temp<30){
+//     console.log("“Today’s Weather is cool.”");
+// }
+// else if(temp>=30 && temp<40){
+//     console.log("“The Weather today is Normal.”");
+// }
+// else if(temp>=40){
+//     console.log("“It is too hot outside.”");
+// }
+
+// else{
+//     console.log("“Please enter a temperature.”");
+// }
+      
+
+ 
+ 
