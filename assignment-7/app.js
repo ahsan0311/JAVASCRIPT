6a659d9b8d9df2d2bac26f7427830c1838a3e051
Ahsan <154538505+ahsan0311@users.noreply.github.com>
2024-04-23 22:12:38 -0700

    chap 14-16

---
+++ assignment-7/app.js
@@ -0,0 +1,92 @@
+
+
+            // question-2
+
+// let stu_name = [prompt("Enter your name")];
+// let each_student_name = [""];
+// each_student_name.push(stu_name)
+
+// let total_marks = [+prompt("Enter your Total marks")];
+// let each_stu_total_marks = [];
+// each_stu_total_marks.push(total_marks);
+
+// let obtained_marks = [+prompt("Enter your Obtained marks")];
+// let each_stu_obt_marks = [];
+// each_stu_obt_marks.push(obtained_marks);
+
+// console.log(`Score of ${each_student_name} is ${each_stu_total_marks - each_stu_obt_marks} and your percentage is ${(each_stu_obt_marks / each_stu_total_marks) * 100}%`);
+
+
+                    // question-3
+
+// let before_sorting = [880,308,290,100,300];
+// console.log("Score of Student Before Sorting " + before_sorting);
+
+// let after_sorting = before_sorting.sort()
+// console.log("Score of Student after Sorting " + after_sorting);
+
+
+
+                // question-4
+ 
+//  let cities_list = ["Karachi" , "Lahore" , "Islamabad" , "Quetta" , "Peshawar"] ;
+// console.log(`This are the cities of Pakistan  ${ cities_list}  but the winner is two cities`) ;
+
+// let selected_cities = cities_list.slice(2,4)
+// console.log(`The winner cities is ${selected_cities}`);
+
+
+            // question-5
+//  console.log("before convert into string")
+//  let  before_string = ["This",  "is", "my", "cat"];
+//  console.log(before_string)
+
+//  console.log("after convert into string")
+//  let after_srting = before_string.toString()
+
+//  console.log(after_srting.replace(/,/g, " "));
+ 
+
+                 // question-6
+ 
+// let devices = ["Keyboard", "Mouse" , "Printer" , "Monitor"];
+// console.log ("Before convert" + devices)
+
+
+// console.log(`After convert \n\nOut:\n${devices[0]} \n\nOut:\n${devices[1]} \n\nOut:\n${devices[2]} \n\nOut:\n${devices[3]}`);
+
+
+                // question-7
+
+// let phone_manufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+
+// document.write("<select>");
+// document.write('<option selected>Phone Manufacturer</option>');
+// document.write('<option value="Apple">' + phone_manufacturers[0] + '</option>');
+// document.write('<option value="Samsung">' + phone_manufacturers[1] + '</option>');
+// document.write('<option value="Motorola">' + phone_manufacturers[2] + '</option>');
+// document.write('<option value="Nokia">' + phone_manufacturers[3] + '</option>');
+// document.write('<option value="Sony">' + phone_manufacturers[4] + '</option>');
+// document.write('<option value="Haier">' + phone_manufacturers[5] + '</option>');
+// document.write("</select>");
+
+                // question-8
+  
+// let stu_name = ["Ayan"] ;
+// let str_arr = ["Nomi"] ;
+// let num_arr = [25] ;
+// let bool_arr = [true];
+// let mix_arr = ["Faiz" , 20 , true];
+
+// let  qualifications_in_Pakistan = ["SSC" , "HSC" , "BSCS" , "BS" , "BCOM" , "MS" , "M.PHIL" , "PHD" ];
+// console.log ("Qualification");
+// for(i=0 ; i<=qualifications_in_Pakistan.length-1 ; i++){
+//     console.log(`${i} ${qualifications_in_Pakistan[i]}`)
+// }
+
+                      
+
+
+
+
+
