9e65edd5c0fbba9b69932bcea754a5381f8c0d98
Ahsan <154538505+ahsan0311@users.noreply.github.com>
2024-04-27 11:02:20 -0700

    assignment-8

--- app.js
+++ app.js
@@ -1,51 +1,47 @@
-// let a = 10;
-// console.log("The value of a is", a);
-// let a_1 = ++a;
-// console.log("After increment the value of a is ", a_1);
+                        // questtion-1
+// for(let i = 1 ; i<=10 ; i++){
+//     console.log(`${i}`);
+// }                        
 
-// let b = 10;
-// console.log("The value of a is", b);
-// let b_1 = --b;
-// console.log("After decrement the value of b is ", b_1);
 
-        // question-2
+                        // questtion-2
 
-//  let a  = 2;
-//  let b = 1;
-//  console.log(--a - --b + ++b + ++a);
- 
-//  console.log("--a =" , --a);
-//  console.log("--a - --b =" , --a - --b);
-//  console.log("--a - --b + ++b =" , --a - --b + ++b );
-//  console.log("--a - --b + ++b + b++ =" , --a - --b + ++b + b++ );
+//   let  user_input_1 = +prompt("Enter a number do you want to multiply");
+//   let user_input_2  = +prompt("Enter a length you want to multiply ");
+//   for(let i=1; i<=user_input_2; i++) {
+//     console.log(`${user_input_1} x ${i}  = ${user_input_1*i}`)
+//   }      
 
-        // question-3
 
-//  let table = +prompt("Enter your fav number", 5);
-//  console.log(table , " x " , " 1 " , " = " , table * 1 , "<br>");       
-//  console.log(table , " x " , " 2 " , " = " , table * 2 , "<br>");       
-//  console.log(table , " x " , " 3 " , " = " , table * 3 , "<br>");       
-//  console.log(table , " x " , " 4 " , " = " , table * 4 , "<br>");       
-//  console.log(table , " x " , " 5 " , " = " , table * 5 , "<br>");       
-//  console.log(table , " x " , " 6 " , " = " , table * 6 , "<br>");       
-//  console.log(table , " x " , " 7 " , " = " , table * 7 , "<br>");       
-//  console.log(table , " x " , " 8 " , " = " , table * 8 , "<br>");       
-//  console.log(table , " x " , " 9 " , " = " , table * 9 , "<br>");       
-//  console.log(table , " x " , " 10 " , " = " , table * 10 , "<br>");        
+                        // questtion-3
 
-            // question-5
+   
+// let fruits = ["apple" , "banana" , "mango" , "orange"];
+// for(let i=0; i<=fruits.length-1; i++){
+//     console.log(`${i} ${fruits[i]}`)
+// }
 
-//  let Math_marks = +prompt("Enter your Math subject marks");           
-//  let Urdu_marks = +prompt("Enter your Urdu subject marks");           
-//  let English_marks = +prompt("Enter your English subject marks");  
-//  let total_marks = 300;
-//  let obtained_marks = Math_marks+Urdu_marks+English_marks;
 
- 
-//  let percentage  = obtained_marks/total_marks*100;
+                        // questtion-3
 
-//  console.log ("English 100 out of" , English_marks);
-//  console.log ("Math 100 out of" , Urdu_marks);
-//  console.log ("Urdu 100 out of" , Math_marks);
-//  console.log("according to your marks your is ", percentage , "%");
+
+// let counting = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] ;
+// for (let i=0; i<counting.length-1; i++){
+//     console.log(`Counting: ${counting[i]}`)
+// }  
+
+// let reverse_counting = [1,2,3,4,5,6,7,8,9,10];
+// for (let i = reverse_counting.length - 1; i >= 0; i--) {
+//     console.log(`Counting: ${reverse_counting[i]}`);
+// }
+
+// let even = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+// for (let i = 0; i < even.length; i++) {
+//     if (even[i] % 2 === 0) {
+//         console.log(`${even[i]} Even`);
+//     } 
+// }
+
+
 
