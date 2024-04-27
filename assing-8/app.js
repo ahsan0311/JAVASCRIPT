33f52f8ba3dc93f5b0ada25ba328870c415797d0
Ahsan <154538505+ahsan0311@users.noreply.github.com>
2024-04-27 11:04:30 -0700

    assignment-8

---
+++ assing-8/app.js
@@ -0,0 +1,47 @@
+                        // questtion-1
+// for(let i = 1 ; i<=10 ; i++){
+//     console.log(`${i}`);
+// }                        
+
+
+                        // questtion-2
+
+//   let  user_input_1 = +prompt("Enter a number do you want to multiply");
+//   let user_input_2  = +prompt("Enter a length you want to multiply ");
+//   for(let i=1; i<=user_input_2; i++) {
+//     console.log(`${user_input_1} x ${i}  = ${user_input_1*i}`)
+//   }      
+
+
+                        // questtion-3
+
+   
+// let fruits = ["apple" , "banana" , "mango" , "orange"];
+// for(let i=0; i<=fruits.length-1; i++){
+//     console.log(`${i} ${fruits[i]}`)
+// }
+
+
+                        // questtion-3
+
+
+// let counting = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] ;
+// for (let i=0; i<counting.length-1; i++){
+//     console.log(`Counting: ${counting[i]}`)
+// }  
+
+// let reverse_counting = [1,2,3,4,5,6,7,8,9,10];
+// for (let i = reverse_counting.length - 1; i >= 0; i--) {
+//     console.log(`Counting: ${reverse_counting[i]}`);
+// }
+
+// let even = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+// for (let i = 0; i < even.length; i++) {
+//     if (even[i] % 2 === 0) {
+//         console.log(`${even[i]} Even`);
+//     } 
+// }
+
+
+
