b2208cb049a2762de822f72dd69338f9e137abcd
Ahsan <154538505+ahsan0311@users.noreply.github.com>
2024-04-29 21:30:29 -0700

    assign-8

---
+++ assing-8/app.js
@@ -0,0 +1,97 @@
+                        // questtion-1
+// for(let i = 1 ; i<=10 ; i++){
+//     console.log(`${i}`);
+// }                        
+
+
+                        // questtion-2
+
+//   let  user_input_1 = +prompt("Enter a number do you want to multiply");
+//   let user_input_2  = +prompt("Enter a length you want to multiply ");
+//   for(let i=1; i<=user_input_2; i++) {
+//     console.log(`${user_input_1} x ${i}  = ${user_input_1*i}`)
+//   }      
+
+
+                        // questtion-3
+
+   
+// let fruits = ["apple" , "banana" , "mango" , "orange"];
+// for(let i=0; i<=fruits.length-1; i++){
+//     console.log(`${i} ${fruits[i]}`)
+// }
+
+
+                        // questtion-3
+
+
+// let counting = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] ;
+// for (let i=0; i<counting.length-1; i++){
+//     console.log(`Counting: ${counting[i]}`)
+// }  
+
+// let reverse_counting = [1,2,3,4,5,6,7,8,9,10];
+// for (let i = reverse_counting.length - 1; i >= 0; i--) {
+//     console.log(`Counting: ${reverse_counting[i]}`);
+// }
+
+// let even = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+// for (let i = 0; i < even.length; i++) {
+//     if (even[i] % 2 === 0) {
+//         console.log(`${even[i]} Even`);
+//     } 
+// }
+
+
+                            // question-4
+
+// let bakrey_items = ["cake", "applepie", "cookie", "chips", "patties"];
+// let user_input = prompt("Enter your favorite bakery item").toLowerCase();
+
+// let is_available = false;
+
+// for (let i = 0; i < bakrey_items.length; i++) {
+//     if (bakrey_items[i] === user_input) {
+//         is_available = true; 
+//         break; 
+// }
+// }
+
+// if (is_available) {
+//     console.log(`Yes ${user_input} is available`);
+// } else {
+//     console.log(`Sorry ${user_input} is not available`);
+// }
+
+                        // question-5
+
+// let num_arr = [24, 55, 78, 96, 3];
+// let largest_number = num_arr[0]; 
+
+// for (let i = 0; i < num_arr.length; i++) {
+//     if (num_arr[i] > largest_number) {
+//         largest_number = num_arr[i]; 
+//     }
+// }
+
+// console.log(`Array numbers ${num_arr}`)
+
+// console.log("The largest number in an array is " + largest_number);
+
+
+                        // question-6
+
+// let num_arr = [24, 55, 78, 96, 3];
+// let smallest_number = num_arr[0]; 
+
+// for (let i = 0; i < num_arr.length; i++) {
+//     if (num_arr[i] < smallest_number) {
+//         smallest_number = num_arr[i]; 
+//     }
+// }
+
+// console.log(`Array numbers ${num_arr}`)
+
+// console.log("The smallest number in an array is " + smallest_number);
+                      
